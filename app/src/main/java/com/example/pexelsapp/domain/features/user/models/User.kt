ec102a42d94d01a3400fb41793662cb831a6c087
Timur Abrashin <timurabr.work@gmail.com>
2026-04-21 17:29:09 +0300

    feat: add user repository

---
+++ app/src/main/java/com/example/pexelsapp/domain/features/user/models/User.kt
@@ -0,0 +1,8 @@
+package com.example.pexelsapp.domain.features.user.models
+
+data class User (
+    val id: String,
+    val email: String,
+    val name: String,
+    val photoUrl: String,
+)
---
+++ app/src/main/java/com/example/pexelsapp/domain/features/user/repositories/UserRepository.kt
@@ -0,0 +1,29 @@
+package com.example.pexelsapp.domain.features.user.repositories
+
+import com.example.pexelsapp.domain.features.user.models.User
+import com.example.pexelsapp.utils.models.Outcome
+import kotlinx.coroutines.flow.StateFlow
+
+interface UserRepository {
+    val user: StateFlow<User?>
+
+    suspend fun getUserById(userId: String): Outcome<User, UserError.Get>
+    suspend fun updateProfile(user: User): Outcome<Unit, UserError.Update>
+}
+
+sealed interface UserError {
+    sealed interface Common : Get, Update {
+        object Network : Common
+        object Server : Common
+        object Unknown : Common
+    }
+
+    sealed interface Get : UserError {
+        object NotFound : Get
+    }
+
+    sealed interface Update : UserError{
+        object InvalidData : Update
+    }
+}
+
